b87c8740bbfc111c21bdc4bb65b350cf93ad362d
Andres Bastidas <andres@bastidas.us>
2025-06-30 00:16:01 -0400

    Setup frontend project with Vue 3, Vite, TailwindCSS, and essential layouts and utilities.

---
+++ frontend/src/router.ts
@@ -0,0 +1,19 @@
+// import {createRouter, createWebHistory} from "vue-router";
+// import AppLayout from "@/components/layouts/AppLayout.vue";
+//
+// const routes = [
+//     {
+//         path: "/",
+//         component: AppLayout,
+//         children: [
+//             { path: '/', name: 'Dashboard', component: Dashboard }
+//         ]
+//     }
+// ]
+//
+// const router = createRouter({
+//     history: createWebHistory(),
+//     routes: []
+// })
+//
+// export default router
