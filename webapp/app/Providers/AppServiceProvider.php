4329ea9e366e538072c2b9e5cc83e9c69cda8072
Andres Bastidas <andres@bastidas.us>
2025-07-11 20:19:11 -0400

    Add HTTPS enforcement in production, integrate htmx.js, and create personal access tokens migration
    - Enforce HTTPS scheme in `AppServiceProvider` for production.
    - Add `htmx.org` library and initialize it in the frontend.
    - Configure new database migration for `personal_access_tokens`.
    - Introduce API route for user retrieval with Sanctum middleware.

--- webapp/app/Providers/AppServiceProvider.php
+++ webapp/app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if ($this->app->isProduction()) {
+            $this->app['request']->server->set('HTTPS', 'on');
+            URL::forceScheme('https');
+        } else {
+            URL::forceScheme('http');
+        }
     }
 }
--- webapp/resources/js/bootstrap.js
+++ webapp/resources/js/bootstrap.js
@@ -1,9 +1,11 @@
 import axios from "axios";
 import Alpine from "alpinejs";
-window.axios = axios;
+import htmx from 'htmx.org';
 
+window.htmx = htmx;
+
+window.axios = axios;
 window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
 
 window.Alpine = Alpine;
-
 Alpine.start();
